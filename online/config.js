2d025d910357ec8342ee717faaffc54de54f2b7e
user-x45 <asukasa462@gmail.com>
2026-08-26 19:07:45 +0900

    Update config.js

--- online/config.js
+++ online/config.js
@@ -1,4 +1,4 @@
 // Cloudflare Workers をデプロイした後に発行される URL に書き換えてください。
 // 例: "https://wordwolf-online.your-subdomain.workers.dev"
 // 独自ドメインを Worker に割り当てた場合はそのドメインを指定してください。
-const WORKER_URL = "https://wordwolf-online.user-x45.workers.dev";
+const WORKER_URL = "https://wolf-online.user-x45.workers.dev";
